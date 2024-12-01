fbf0b69eb4258774f924b730a356eae7f023cd30
pavelbereza77 <rabotam2013@yandex.ru>
2024-12-01 13:20:47 +0300

    commit nuxtjs_blog

---
+++ layouts/post_detail.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Navbar />
+    <Nuxt />
+
+    <Footer/>
+
+  </div>
+</template>
+
+
+<script>
+import Navbar from "~/components/Navbar";
+import Footer from "~/components/Footer";
+
+export default {
+  components: {
+
+    Navbar,
+    Footer,
+  },
+  head() {
+    return {
+      link: [
+        { rel: "canonical", href: `http://localhost:3000${this.$route.path}`}
+      ]
+    }
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ pages/example.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <ExampleComponent @sendTopage="data_from = $event"/>
+      <h1>Это страница - родитель</h1>
+      <p>Это данные из компонента <b>{{ data_from }}</b></p>
+    </div>
+  </template>
+
+  <script>
+  import ExampleComponent from "@/components/ExampleComponent";
+  export default {
+    components: {
+      ExampleComponent
+    },
+    data() {
+      return {
+        data_from: ""
+      }
+    }
+  }
+  </script>
+
+  <style scoped>
+
+  </style>
